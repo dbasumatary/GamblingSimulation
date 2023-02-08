9f0b385892d43b8831e7d72c2a76b07cf0c41c25
dbasumatary <basudhondip@gmail.com>
2023-02-09 04:11:48 +0530

    Added: Win or lose the game by making a bet

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,10 +1,16 @@
 package com.gamblingsimulation;
-/*Starting attributes initialized for the program*/
+/*Making a bet with $1*/
 public class GamblingSimulation {
     public static final int EveryDayStake = 100;    //Initializing static variable
     public static final int betEveryGame = 1;      //Initializing static variable
-
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulator Program");
+        int random = (int) Math.floor(Math.random()*10) % 2;
+        if (random == betEveryGame){                           //Checking if the gambler won the bet
+            System.out.println("You won the game");
+        }
+        else{
+            System.out.println("You lost the game");
+        }
     }
 }
