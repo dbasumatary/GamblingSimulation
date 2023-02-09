fc8ee5281ad0d037a4a88f225952472c524e4795
dbasumatary <basudhondip@gmail.com>
2023-02-09 15:07:21 +0530

    Added a method to see the outcome of first bet

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,16 +1,24 @@
 package com.gamblingsimulation;
-/*Making a bet with $1*/
+/*Making a bet with $1 and observing the outcome*/
 public class GamblingSimulation {
-    public static final int EveryDayStake = 100;    //Initializing static variable
-    public static final int betEveryGame = 1;      //Initializing static variable
-    public static void main(String[] args) {
-        System.out.println("Welcome To Gambling Simulator Program");
-        int random = (int) Math.floor(Math.random()*10) % 2;
-        if (random == betEveryGame){                           //Checking if the gambler won the bet
-            System.out.println("You won the game");
+    public static final int EveryDayStake = 100;                    //Initializing static variable (UC1)
+    public static final int betEveryGame = 1;                       //Initializing static variable (UC1)
+    /*Creating a new method to see if gambler won or lost (UC2)*/
+    public void gamblingWinLose(){
+        double random = Math.random();
+        int remainingAmount = EveryDayStake;         //Before the bet the remaining amount and total stake is same
+        if (random>0.5){
+            remainingAmount += betEveryGame;
+            System.out.println("The Gambler won the first bet. He has $" +remainingAmount+ " remaining with him.");
         }
         else{
-            System.out.println("You lost the game");
+            remainingAmount -= betEveryGame;
+            System.out.println("The Gambler lost the first bet. He has $" +remainingAmount+ " remaining with him.");
         }
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulator Program");
+        GamblingSimulation simulation = new GamblingSimulation();                 //Creating a new object
+        simulation.gamblingWinLose();                                           //Calling the method
     }
 }
