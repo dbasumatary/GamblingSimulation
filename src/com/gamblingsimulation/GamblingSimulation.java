327a867aaa34c8e76e56e2df232104c0b782e183
dbasumatary <basudhondip@gmail.com>
2023-02-09 15:49:57 +0530

    Added a method for gambler to resign

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,16 +1,42 @@
 package com.gamblingsimulation;
-/*Making a bet with $1*/
+/*Gambler would resign for the day if won or lose 50% of stake*/
 public class GamblingSimulation {
-    public static final int EveryDayStake = 100;    //Initializing static variable
-    public static final int betEveryGame = 1;      //Initializing static variable
-    public static void main(String[] args) {
-        System.out.println("Welcome To Gambling Simulator Program");
-        int random = (int) Math.floor(Math.random()*10) % 2;
-        if (random == betEveryGame){                           //Checking if the gambler won the bet
-            System.out.println("You won the game");
+    public static final int EveryDayStake = 100;                    //Initializing static variable for total stack (UC1)
+    public static final int betEveryGame = 1;                       //Initializing static variable for every bet (UC1)
+    /*Creating a new method to see if gambler won or lost (UC2)*/
+    public void gamblingWinLose(){
+        double random = Math.random();
+        int remainingAmount = EveryDayStake;         //Before the bet the remaining amount and total stake is same
+        if (random > 0.5){
+            remainingAmount += betEveryGame;
+            System.out.println("The Gambler won the first bet. He has $" +remainingAmount+ " remaining with him.");
         }
         else{
-            System.out.println("You lost the game");
+            remainingAmount -= betEveryGame;
+            System.out.println("The Gambler lost the first bet. He has $" +remainingAmount+ " remaining with him.");
         }
+    }
+    /*Creating a new method to print that gambler would resign for the day if won or lose 50% of stake*/
+    public void gamblerResign(int margin){
+        double highAmount = EveryDayStake + margin*EveryDayStake/100;            //Upper margin for resigning
+        double lowAmount = EveryDayStake - margin*EveryDayStake/100;             //Lower margin for resigning
+        double availableAmount = EveryDayStake;                                   //Before the bet they will be same
+        while ((availableAmount < highAmount) && (availableAmount > lowAmount)){
+            double random = Math.random();
+            if (random > 0.5){                                           //Using random to check if gambler win or lose
+                availableAmount += betEveryGame;
+            }
+            else{
+                availableAmount -= betEveryGame;
+            }
+        }
+        System.out.println("The Gambler resigns for the day. He has $" +availableAmount+ " remaining with him.");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulator Program");
+        GamblingSimulation simulation = new GamblingSimulation();                 //Creating a new object
+        simulation.gamblingWinLose();                                            //Calling the first method (UC2)
+        GamblingSimulation resignCheck = new GamblingSimulation();
+        resignCheck.gamblerResign(50);                                //Calling the second method (UC3)
     }
 }
